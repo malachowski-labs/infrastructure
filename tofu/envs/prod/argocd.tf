1a8437ea191444d1c2467402bda341a1742b51fe
Kacper Małachowski <38684517+KacperMalachowski@users.noreply.github.com>
2026-02-05 07:38:47 +0100

    fix: finish cluster setup (#15)

--- tofu/envs/prod/argocd.tf
+++ tofu/envs/prod/argocd.tf
@@ -3,22 +3,22 @@ data "google_secret_manager_secret_version" "argocd_app_pem_key" {
 }
 
 resource "kubernetes_namespace_v1" "argocd" {
-  depends_on = [ module.talos ]
+  depends_on = [module.talos, hcloud_load_balancer_service.this]
 
   metadata {
     name = "argocd"
 
     labels = {
-      name = "argocd"
+      name                                 = "argocd"
       "pod-security.kubernetes.io/enforce" = "privileged"
     }
   }
 }
 
 resource "helm_release" "argocd" {
-  depends_on = [module.talos, kubernetes_namespace_v1.argocd]
+  depends_on = [module.talos, kubernetes_namespace_v1.argocd, hcloud_load_balancer_service.this]
   name       = "argocd"
-  namespace = kubernetes_namespace_v1.argocd.metadata[0].name
+  namespace  = kubernetes_namespace_v1.argocd.metadata[0].name
 
   repository = "https://argoproj.github.io/argo-helm"
   chart      = "argo-cd"
@@ -28,9 +28,9 @@ resource "helm_release" "argocd" {
 }
 
 resource "kubernetes_secret_v1" "argocd_repo_access" {
-  depends_on = [module.talos, helm_release.argocd, kubernetes_namespace_v1.argocd ]
+  depends_on = [module.talos, helm_release.argocd, kubernetes_namespace_v1.argocd]
   metadata {
-    name = "argo-private-repo-secret"
+    name      = "argo-private-repo-secret"
     namespace = kubernetes_namespace_v1.argocd.metadata[0].name
 
     labels = {
@@ -48,14 +48,14 @@ resource "kubernetes_secret_v1" "argocd_repo_access" {
 }
 
 resource "argocd_application" "cluster_config" {
-  depends_on = [ kubernetes_secret_v1.argocd_repo_access, helm_release.argocd, kubernetes_namespace_v1.argocd ]
+  depends_on = [kubernetes_secret_v1.argocd_repo_access, helm_release.argocd, kubernetes_namespace_v1.argocd]
 
   metadata {
     name      = "cluster-config"
     namespace = kubernetes_namespace_v1.argocd.metadata[0].name
   }
 
-  wait = true
+  wait = false
 
   spec {
     destination {
@@ -63,8 +63,8 @@ resource "argocd_application" "cluster_config" {
     }
 
     source {
-      repo_url = "https://github.com/malachowski-labs/manifests"
-      path = "clusters/prod.malachowski.me"
+      repo_url        = "https://github.com/malachowski-labs/manifests"
+      path            = "clusters/prod.malachowski.me"
       target_revision = "HEAD"
 
       directory {
@@ -74,7 +74,7 @@ resource "argocd_application" "cluster_config" {
 
     sync_policy {
       automated {
-        prune = true
+        prune     = true
         self_heal = true
       }
 
@@ -87,10 +87,10 @@ resource "argocd_application" "cluster_config" {
 }
 
 resource "argocd_application" "traefik" {
-  depends_on = [ helm_release.argocd, kubernetes_namespace_v1.argocd, hcloud_load_balancer.this ]
+  depends_on = [helm_release.argocd, kubernetes_namespace_v1.argocd, hcloud_load_balancer.this]
 
   metadata {
-    name = "argo-traefik-chart"
+    name      = "argo-traefik-chart"
     namespace = kubernetes_namespace_v1.argocd.metadata[0].name
   }
 
@@ -102,8 +102,8 @@ resource "argocd_application" "traefik" {
     }
 
     source {
-      repo_url = "https://github.com/traefik/traefik-helm-chart.git"
-      path = "traefik"
+      repo_url        = "https://github.com/traefik/traefik-helm-chart.git"
+      path            = "traefik"
       target_revision = "v38.0.2"
 
       helm {
@@ -123,7 +123,7 @@ resource "argocd_application" "traefik" {
 
     sync_policy {
       automated {
-        prune = true
+        prune     = true
         self_heal = true
       }
 
